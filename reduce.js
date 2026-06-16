9bf4cdaa6b648b93b695a255f9bdf4b5ee0dc983
Abduvohid <abduvohid24082007@gmail.com>
2026-06-16 22:21:18 +0500

    yangi fayl va kodlar qoshildi

--- reduce.js
+++ reduce.js
@@ -1,83 +1,83 @@
-//  Massivdagi musbat sonlar yig’indisini hisoblang. (filter va
-// reduce)
-// Input: [ 1, -4, 12, 0, -3, 29, -150]
-// Output: 42
-const raqamlar = [1, -4, 12, 0, -3, 29, -150];
+// //  Massivdagi musbat sonlar yig’indisini hisoblang. (filter va
+// // reduce)
+// // Input: [ 1, -4, 12, 0, -3, 29, -150]
+// // Output: 42
+// const raqamlar = [1, -4, 12, 0, -3, 29, -150];
 
-const natijas = raqamlar.filter((r) => r > 0).reduce((sum, r) => sum + r, 0);
+// const natijas = raqamlar.filter((r) => r > 0).reduce((sum, r) => sum + r, 0);
 
-console.log(natijas);
-// ================================================================================
-// 12. Barcha products narxlari yig'indisi topilsin. (reduce)
+// console.log(natijas);
+// // ================================================================================
+// // 12. Barcha products narxlari yig'indisi topilsin. (reduce)
 
-const products = [
-  {
-    id: 6,
-    name: "Smarthpone",
-    price: 12000,
-    rating: 4.5,
-    discount: 20,
-  },
-  {
-    id: 2,
-    name: "Acer",
-    price: 10000,
-    rating: 4.3,
-    discount: 10,
-  },
-  {
-    id: 1,
-    name: "Mac book",
-    price: 17000,
-    rating: 4.7,
-    discount: 40,
-  },
-  {
-    id: 4,
-    name: "HP",
-    price: 21000,
-    rating: 4.1,
-    discount: 30,
-  },
-  {
-    id: 5,
-    name: "Dell",
-    price: 35000,
-    rating: 4.9,
-    discount: 30,
-  },
-];
-const natija = products.reduce((acc, el) => acc + el.price, 0);
-console.log(natija);
-// ========================================================
-// Quyidagi pupils massividagi barcha o'quvchilarni
-// protcentlarining o'rtacha qiymatini toping. (reduce)
-const pupils = [
-  {
-    name: "Elbek",
-    protcent: 95,
-  },
-  {
-    name: "Zafar",
-    protcent: 78,
-  },
-  {
-    name: "Aziz",
-    protcent: 83,
-  },
-  {
-    name: "Jasur",
-    protcent: 88,
-  },
-  {
-    name: "Bobur",
-    protcent: 66,
-  },
-  {
-    name: "Kamron",
-    protcent: 75,
-  },
-];
-const jami = pupils.reduce((acc, el) => acc + el.protcent, 0);
-console.log(jami);
-// ============================================================
+// const products = [
+//   {
+//     id: 6,
+//     name: "Smarthpone",
+//     price: 12000,
+//     rating: 4.5,
+//     discount: 20,
+//   },
+//   {
+//     id: 2,
+//     name: "Acer",
+//     price: 10000,
+//     rating: 4.3,
+//     discount: 10,
+//   },
+//   {
+//     id: 1,
+//     name: "Mac book",
+//     price: 17000,
+//     rating: 4.7,
+//     discount: 40,
+//   },
+//   {
+//     id: 4,
+//     name: "HP",
+//     price: 21000,
+//     rating: 4.1,
+//     discount: 30,
+//   },
+//   {
+//     id: 5,
+//     name: "Dell",
+//     price: 35000,
+//     rating: 4.9,
+//     discount: 30,
+//   },
+// ];
+// const natija = products.reduce((acc, el) => acc + el.price, 0);
+// console.log(natija);
+// // ========================================================
+// // Quyidagi pupils massividagi barcha o'quvchilarni
+// // protcentlarining o'rtacha qiymatini toping. (reduce)
+// const pupils = [
+//   {
+//     name: "Elbek",
+//     protcent: 95,
+//   },
+//   {
+//     name: "Zafar",
+//     protcent: 78,
+//   },
+//   {
+//     name: "Aziz",
+//     protcent: 83,
+//   },
+//   {
+//     name: "Jasur",
+//     protcent: 88,
+//   },
+//   {
+//     name: "Bobur",
+//     protcent: 66,
+//   },
+//   {
+//     name: "Kamron",
+//     protcent: 75,
+//   },
+// ];
+// const jami = pupils.reduce((acc, el) => acc + el.protcent, 0);
+// console.log(jami);
+// // ============================================================
---
+++ vazifa4.js
@@ -0,0 +1,102 @@
+let students = [
+  { id: 1, name: "Ali", age: 20 },
+  { id: 2, name: "Vali", age: 22 },
+];
+
+function qoshamiz(id, name, age) {
+  const newuser = {
+    id: students.length + 1,
+    name: name,
+    age: age,
+  };
+  students.push(newuser);
+  return newuser;
+}
+
+console.log(qoshamiz(3, "Gani", 19));
+console.log(students);
+
+function getStudents() {
+  students.forEach((student) => {
+    console.log(
+      `idsi ${student.id}, ismi ;${student.name} yoshi :${student.age}`,
+    );
+  });
+}
+getStudents();
+
+let students = [
+  { id: 1, name: "Ali", age: 20 },
+  { id: 2, name: "Vali", age: 22 },
+];
+function updateAge(id, newAge) {
+  const natija = students.find((student) => student.id === id);
+  if (natija) {
+    natija.age = newAge;
+  }
+}
+updateAge(1, 25);
+console.log(students);
+
+let students = [
+  { id: 1, name: "Ali", age: 20 },
+  { id: 2, name: "Vali", age: 22 },
+  { id: 3, name: "Hasan", age: 19 },
+];
+
+function deleteStudent(id) {
+  students = students.filter(function (student) {
+    return student.id !== id;
+  });
+}
+
+deleteStudent(2);
+
+console.log(students);
+
+let carts = [
+  {
+    id: 1,
+    name: "telefon",
+    price: 300,
+    quantity: 5,
+  },
+  {
+    id: 2,
+    name: "quloqchin",
+    price: 70,
+    quantity: 8,
+  },
+  {
+    id: 3,
+    name: "powerbank",
+    price: 50,
+    quantity: 15,
+  },
+];
+function addToCart(product) {
+  let maxsulot = carts.find((cart) => cart.id === product.id);
+  if (maxsulot) {
+    maxsulot.quantity += product.quantity;
+  } else {
+    carts.push(product);
+  }
+}
+
+addToCart({ id: 1, quantity: 2 });
+console.log(carts);
+
+function changeQuantity(id, count) {
+  let tovar = carts.find((cart) => cart.id === id);
+  if (tovar) {
+    tovar.quantity = count;
+  }
+}
+changeQuantity(2, 10);
+console.log(carts);
+
+function removeFromCart(id) {
+  carts = carts.filter((cart) => cart.id !== id);
+}
+removeFromCart(2);
+console.log(carts);
